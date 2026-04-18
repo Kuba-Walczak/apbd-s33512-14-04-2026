be3c71ad7c7bff2c8103a6b85038385c0d883d07
Jacob <kubawalczak005@gmail.com>
2026-04-18 10:25:36 +0200

    feat: added model validation

--- task5/Models/Reservation.cs
+++ task5/Models/Reservation.cs
@@ -1,14 +1,26 @@
+using System.ComponentModel.DataAnnotations;
 using task5.Enums;
 
 namespace task5.Models;
 
-public class Reservation {
+public class Reservation : IValidatableObject {
     public int Id { get; set; }
     public int RoomId { get; set; }
-    public string OrganizerName { get; set; }
-    public string Topic { get; set; }
+
+    [Required]
+    public string OrganizerName { get; set; } = null!;
+
+    [Required]
+    public string Topic { get; set; } = null!;
+
     public DateOnly Date { get; set; }
     public TimeOnly StartTime { get; set; }
     public TimeOnly EndTime { get; set; }
     public ReservationStatus Status { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        if (EndTime <= StartTime)
+            yield return new ValidationResult("EndTime must be greater than or equal to StartTime");
+    }
 }
--- task5/Models/Room.cs
+++ task5/Models/Room.cs
@@ -1,13 +1,22 @@
+using System.ComponentModel.DataAnnotations;
 using task5.Enums;
 
 namespace task5.Models;
 
 public class Room {
     public int Id { get; set; }
-    public string Name { get; set; }
-    public string BuildingCode { get; set; }
+
+    [Required]
+    public string Name { get; set; } = null!;
+
+    [Required]
+    public string BuildingCode { get; set; } = null!;
+
     public int Floor { get; set; }
+
+    [Range(1, int.MaxValue)]
     public int Capacity { get; set; }
+
     public bool HasProjector  { get; set; }
     public bool IsActive { get; set; }
 }
